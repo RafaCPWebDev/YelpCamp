3b47c06002449ce6beb6fa07a8952fa71409c26b
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2019-01-19 13:15:12 +0000

    camp prices added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,13 +14,14 @@ router.get("/", function(req, res){
 //CREATE
 router.post("/", middleware.isLoggedIn, function(req,res){
     var name = req.body.name;
+    var price = req.body.price;
     var image = req.body.image;
     var description = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     }
-    var newCampground = {name:name, image: image, description: description, author: author};
+    var newCampground = {name:name, price: price, image: image, description: description, author: author};
     Campground.create(newCampground, function(err, campground){
         if(err) console.log(err);
         else res.redirect("/campgrounds");
